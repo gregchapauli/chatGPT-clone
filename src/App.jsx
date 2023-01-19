04d7f9b0d1680c89e2f97972d0229b4ed067c809
Gregory Caloin <gregchapauli@gmail.com>
2023-01-19 22:47:30 +0100

    Translation component creation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import "./App.css";
 import OptionSelection from "./components/OptionSelection";
+import Translation from "./components/Translation";
 import { arrayItems } from "./AIOptions";
+import { useState } from "react";
 
 function App() {
+  const [option, setOption] = useState({});
   //console.log(import.meta.env.VITE_Open_AI_Key);
   const selectOption = (option) => {
-    console.log(option);
+    setOption(option);
   };
-
+  console.log(Object.values(option).length);
   return (
     <h1 className="App">
-      <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      {Object.values(option).length === 0 ? (
+        <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+      ) : (
+        <Translation />
+      )}
     </h1>
   );
 }
--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Translation() {
-  return <div>Translation</div>;
+  return (
+    <div>
+      <textarea className="text-area" cols={55} rows={10}></textarea>
+      <button className="action-btn">DO YOUR STUFF!</button>
+    </div>
+  );
 }
