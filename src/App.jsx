6642fc0f9319fd69d242272f4227b5dc5e466f76
Gregory Caloin <gregchapauli@gmail.com>
2023-01-19 23:00:41 +0100

    text area action created / openai installed

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import { useState } from "react";
 
 function App() {
   const [option, setOption] = useState({});
+  const [input, setInput] = useState("");
   //console.log(import.meta.env.VITE_Open_AI_Key);
   const selectOption = (option) => {
     setOption(option);
   };
-  console.log(Object.values(option).length);
+  const doStuff = () => {
+    setOption({ ...option, prompt: input });
+  };
+  console.log(option);
   return (
     <h1 className="App">
       {Object.values(option).length === 0 ? (
         <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
       ) : (
-        <Translation />
+        <Translation doStuff={doStuff} setInput={setInput} />
       )}
     </h1>
   );
--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-export default function Translation() {
+export default function Translation({ doStuff, setInput }) {
   return (
     <div>
-      <textarea className="text-area" cols={55} rows={10}></textarea>
-      <button className="action-btn">DO YOUR STUFF!</button>
+      <textarea
+        className="text-area"
+        cols={55}
+        rows={10}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <button className="action-btn" onClick={doStuff}>
+        DO YOUR STUFF!
+      </button>
     </div>
   );
 }
