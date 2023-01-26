fd1452918795786b29a7551e719995875290fb75
Gregory Caloin <gregchapauli@gmail.com>
2023-01-26 23:17:49 +0100

    add typescipter

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 
 export default function Translation({ doStuff, setInput, result }) {
+  const title = document.querySelector("h3");
+
+  function typewriter(txt) {
+    // if (index < txt.length) {
+    //   title.innerHTML += `<span>${txt[index]}</span>`;
+    //   setTimeout(() => {
+    //     typewriter(txt, index + 1);
+    //   }, 100);
+    // }
+    for (let i = 0; i < txt.length; i++) {
+      setTimeout(() => {
+        title.innerHTML += `<span>${txt[i]}</span>`;
+      }, 1000);
+    }
+  }
+  typewriter(result);
+
   return (
     <div className="app">
       <textarea
@@ -13,7 +30,7 @@ export default function Translation({ doStuff, setInput, result }) {
         Ask me something!
       </button>
 
-      <h3 className="result-text">{result.length > 0 ? result : ""}</h3>
+      <h3 className="result-text">{/* {result.length > 0 ? result : ""} */}</h3>
     </div>
   );
 }
